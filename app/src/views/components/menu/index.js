bed4e68d0ef8f3f493badda78dbefbd0ccccc0ae
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-08-08 23:29:23 +0300

    menu item name fix

--- app/src/views/components/menu/index.js
+++ app/src/views/components/menu/index.js
@@ -11,11 +11,11 @@ class menuObject {
   rows() {
     if (!this.items) return;
     return this.items.map((item, index) => {
-      const {menu, href} = item;
+      const {name, href} = item;
       const link = href ? `/${href}` : 'javascript:;';
       return `
         <div class="menu__item">
-          <a href="${link}" data-element="${index}" tabindex="${index + 1}">${menu}</a>
+          <a href="${link}" data-element="${index}" tabindex="${index + 1}">${name}</a>
         </div>
       `
     }).join('');
